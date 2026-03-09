b453cf8123cb2a35f9e22761855cbf8793aa7ed1
sakib228 <sakinhossainmd1@gmail.com>
2026-03-10 00:35:50 +0600

    responsive degine and using javascript to go home page

--- login.js
+++ login.js
@@ -1,2 +1,23 @@
-console.log("Login page");
+// console.log("Login page loaded");
 
+// 1. Add event listener to the login form
+document.getElementById('login_btn').addEventListener('click', function () {
+  const userNameInput = document.getElementById('User_name');
+  const inputValue = userNameInput.value;
+  console.log(inputValue);
+  const userPasswordInput = document.getElementById('User_password');
+  const passwordValue = userPasswordInput.value;
+  console.log(passwordValue);
+
+  if (inputValue === 'admin' && passwordValue === 'admin123') {
+    alert('Login successful!');
+    window.location.assign("/home.html");
+  } else {
+    alert('Invalid username or password.');
+    return;
+  }
+
+});
+
+// 2.get Elements by id of 2 inputs
+// 3.check condition to login
